92b259463882ab766e318e06aaeed6748127db09
arielsepang93-commits <arielsepang93@gmail.com>
2026-09-25 23:05:10 +0800

    Delete api/logout.js

--- api/logout.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader('Set-Cookie', 'authToken=; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0');
-  return res.status(200).json({ success: true, message: 'Logout berhasil' });
-}
